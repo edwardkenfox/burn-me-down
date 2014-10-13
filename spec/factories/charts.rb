2957047453e18f166f320149b50c9b7d31eb425d
Edward <edwardkenfox@gmail.com>
2014-10-13 19:22:32 +0900

    Annotate models

--- spec/factories/charts.rb
+++ spec/factories/charts.rb
@@ -4,7 +4,6 @@
 #
 #  id          :integer          not null, primary key
 #  name        :string(255)
-#  issue_id    :integer
 #  velocity_id :integer
 #  created_at  :datetime
 #  updated_at  :datetime
