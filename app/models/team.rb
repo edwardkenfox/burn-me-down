5ebb1bce61587f552f6569cc533a93e9d021809b
Edward <edwardkenfox@gmail.com>
2014-10-13 13:58:21 +0900

    Add has_many through relations

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team < ActiveRecord::Base
   has_many :team_members
   has_many :users, through: :team_members
   has_many :charts
+  has_many :issues, through: :charts
+  has_many :velocities, through: :charts
 
   validates_presence_of :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :team_users
+  has_many :teams, through: :team_users
 end
