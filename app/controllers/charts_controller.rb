a33d32189e44161c70149b6daa36f8302a1368bd
Edward <edwardkenfox@gmail.com>
2014-10-18 16:09:31 +0900

    Fix authorization for ChartsController

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -92,7 +92,7 @@ class ChartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_chart
-      @chart = Chart.find(params[:id])
+      @chart = policy_scope(Chart).find(params[:id])
       authorize @chart
     end
 
